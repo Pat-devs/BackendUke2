386475a90bc2c84ede4fed670a1340d92f68f77c
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-03 10:51:41 +0200

    for looping through a string

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿// Loops intro
+﻿Console.Clear();
+// Loops intro
 
-string word = "morning";
+string word = "morn"; // 4 characters.... dvs. 0,1,2,3
+// indexes are 0 based, not 1 based
+
+
+Console.WriteLine(word.Length - 1); // Gets the number of characters in the current string object.
 
 //Console.WriteLine(word[100]);
 
-for (int i = 0; i < word.Length; i++)
+for (int i = word.Length - 1; i >= 0; i--)
 {
-    Console.WriteLine(word[i]);
+    Console.Write(word[i]); 
 }
