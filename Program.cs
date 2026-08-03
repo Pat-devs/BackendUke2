c88361bc26e02dca849f2462d379b1c62f47e97c
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-03 09:50:53 +0200

    while loop example

--- Program.cs
+++ Program.cs
@@ -2,13 +2,18 @@
 
 
 // Say hello 10 times.
-Console.WriteLine("hello");
-Console.WriteLine("hello");
-Console.WriteLine("hello");
-Console.WriteLine("hello");
-Console.WriteLine("hello");
-Console.WriteLine("hello");
-Console.WriteLine("hello");
-Console.WriteLine("hello");
-Console.WriteLine("hello");
-Console.WriteLine("hello");
+
+// using a while loop 
+
+int counter = 0;
+
+while (counter < 10) // loop as long as counter value is above 0
+{
+    // do somthing
+
+    // Console.WriteLine(counter);
+    Console.WriteLine("hello");
+
+    // count!!
+    counter++;
+}
