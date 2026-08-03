d421771a224660d74a02bbf6e0d4b45242b7a513
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-03 09:43:04 +0200

    Pre intro to loops: "say hello x times"

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
 ﻿// Loops intro
+
+
+// Say hello 10 times.
+Console.WriteLine("hello");
+Console.WriteLine("hello");
+Console.WriteLine("hello");
+Console.WriteLine("hello");
+Console.WriteLine("hello");
+Console.WriteLine("hello");
+Console.WriteLine("hello");
+Console.WriteLine("hello");
+Console.WriteLine("hello");
+Console.WriteLine("hello");
