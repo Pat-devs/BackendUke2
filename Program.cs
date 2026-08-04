a769aed9cc4bc1f470d7cdf30cb7a48c3e1c0461
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-04 09:42:02 +0200

    Array example use

--- Program.cs
+++ Program.cs
@@ -1,52 +1,11 @@
 ﻿Console.Clear();
 
-string password = "aBc1234jio0i";
-// password requirements:
+// string products = "Coffee, Tea, Bread, Milk";
 
-bool hasNumber = false; // needs to contain a number
-bool moreThan8Characters = false;  // needs to contain a number
-bool containsUpperCaseChar = false;  // needs to contain an uppercase character
-bool containsLowerCaseChar = false;  // needs to contain a lowercase character
+string[] products = { "Coffee", "Tea", "Bread", "Milk" }; // fixed type (f.eks: string), fixed length
 
-// go through each character and check if password is "strong"
-foreach (char character in password)
+// print all products
+foreach (string product in products)
 {
-    if (char.IsDigit(character))
-    {
-        hasNumber = true;
-    }
-    if (char.IsUpper(character))
-    {
-        containsUpperCaseChar = true;
-    }
-    if (char.IsLower(character))
-    {
-        containsLowerCaseChar = true;
-    }
-}
-
-// check length of password
-if (password.Length >= 8)
-{
-    moreThan8Characters = true;
-}
-
-
-// print password strengh summary
-if (!hasNumber)
-{
-    Console.WriteLine("Password must contain a number!");
-}
-
-if (!moreThan8Characters)
-{
-    Console.WriteLine("Password must contain 8 chars or more.");
-}
-if (!containsUpperCaseChar)
-{
-    Console.WriteLine("Password must contain an uppercase character.");
-}
-if (!containsLowerCaseChar)
-{
-    Console.WriteLine("Password must contain a lowercase character.");
+    Console.WriteLine(product);
 }
