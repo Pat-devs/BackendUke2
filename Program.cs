2e65f1dd1f2bd16c6e5c144af2288278640de8e5
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-04 09:14:47 +0200

    password strength v1

--- Program.cs
+++ Program.cs
@@ -1,27 +1,25 @@
 ﻿Console.Clear();
-/* Fizz buzz rules:
 
-1. go through numbers 1-100
-2. If a number is divisible by 3, the student says “fizz” rather than the number.
-3. If a number is divisible by 5, they say “buzz” rather than the number.
-4. If a number is divisible by both, they say “fizz buzz”
-5. In all other cases just print out the number
+string password = "veryeasy0password";
+// password requirements:
 
-*/
+bool hasNumber = false;
 
-// Fizz buzz example soltion
-
-for (int i = 1; i <= 100; i++)
+// go through each character and check if password is "strong"
+foreach (char character in password)
 {
-    Console.Write(i + ". ");
-    if (i % 3 == 0) // if no rest after division it means number is divisble by 3
+    if (char.IsDigit(character))
     {
-        Console.Write("Fizz");
+        hasNumber = true;
     }
-    if (i % 5 == 0)
-    {
-        Console.Write("Buzz");
-    }
+}
 
-    Console.WriteLine();
+// print password strengh summary
+if (hasNumber)
+{
+    Console.WriteLine("Password contains a number.");
+}
+else 
+{
+    Console.WriteLine("Password must contain a number!");
 }
