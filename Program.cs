f3912eca278b3edb09a664131a22d7402ff8c966
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-04 09:31:22 +0200

    password strength with more checks

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿Console.Clear();
 
-string password = "veryeasy0password";
+string password = "aBc1234jio0i";
 // password requirements:
 
-bool hasNumber = false;
+bool hasNumber = false; // needs to contain a number
+bool moreThan8Characters = false;  // needs to contain a number
+bool containsUpperCaseChar = false;  // needs to contain an uppercase character
+bool containsLowerCaseChar = false;  // needs to contain a lowercase character
 
 // go through each character and check if password is "strong"
 foreach (char character in password)
@@ -12,14 +15,38 @@ foreach (char character in password)
     {
         hasNumber = true;
     }
+    if (char.IsUpper(character))
+    {
+        containsUpperCaseChar = true;
+    }
+    if (char.IsLower(character))
+    {
+        containsLowerCaseChar = true;
+    }
 }
 
-// print password strengh summary
-if (hasNumber)
+// check length of password
+if (password.Length >= 8)
 {
-    Console.WriteLine("Password contains a number.");
+    moreThan8Characters = true;
 }
-else 
+
+
+// print password strengh summary
+if (!hasNumber)
 {
     Console.WriteLine("Password must contain a number!");
+}
+
+if (!moreThan8Characters)
+{
+    Console.WriteLine("Password must contain 8 chars or more.");
+}
+if (!containsUpperCaseChar)
+{
+    Console.WriteLine("Password must contain an uppercase character.");
+}
+if (!containsLowerCaseChar)
+{
+    Console.WriteLine("Password must contain a lowercase character.");
 }
