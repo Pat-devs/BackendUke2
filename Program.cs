b372a2344ab21d64fcb65e672d9e9ee8b4c2e4b1
Pat-devs <95124571+Pat-devs@users.noreply.github.com>
2026-08-06 09:33:57 +0200

    create method showallproducts (with error)

--- Program.cs
+++ Program.cs
@@ -1,56 +1,5 @@
 ﻿Console.Clear();
 
-
-
-// statisk funksjon som ikke returnerer noenting
-// statisk funksjon som returnerer void
-
-// basic greeting function:
-static void Greet()
-{
-    Console.WriteLine("Hello world!");
-}
-
-// invoke a function:
-// Greet();
-
-// custom greeting 
-
-static void CustomGreet(string message1, string message2)
-{
-    Console.Write(message1 + " ");
-    Console.Write(message2 + "\n");
-}
-
-// CustomGreet("Hei verden!", "hadet verden");
-// CustomGreet("Hvordan går det?", "Jo, det går fint.");
-
-// a method that takes a text and a number as paramters so that it can print that message a number of times.
-
-// example use: GreetNumberOfTimes("hei", 5) // output: hei, hei, hei, hei, hei
-
-static void GreetNumberOfTimes(string text, int repeatTimes)
-{
-    // for loop solution
-    for (int i = 0; i < repeatTimes; i++)
-    {
-        // Console.Write(text + ", ");  
-    } 
-    // while loop solution
-    while(repeatTimes > 0)
-    {
-        Console.Write(text + ", ");  
-        repeatTimes--;
-    }
-}
-
-GreetNumberOfTimes("hei", 5);
-
-
-
-
-Console.ReadLine(); // for å stopppe resten av koden :)
-
 // Products available:
 string[] products = { "Coffee", "Tea", "Bread", "Milk" }; // fixed type (f.eks: string), fixed length
 
@@ -68,22 +17,16 @@ while(true)
     Console.WriteLine("Products menu (enter a number)");
     Console.WriteLine("1. List all products");
     Console.WriteLine("2. List items in your cart");
-    Console.WriteLine("3. Exit");
+    Console.WriteLine("3. Add an item to cart");
+    Console.WriteLine("4. Remove an item from cart");
+    Console.WriteLine("5. Remove all items from your cart");
+    Console.WriteLine("6. Exit");
 
     string command = Console.ReadLine(); // prompt user for input
 
     if (command == "1")
     {
-        Console.Clear();
-        Console.WriteLine("Our products:");
-
-        foreach (string product in products)
-        {
-            Console.WriteLine(product);
-        }
-
-        Console.WriteLine("Press enter to return to main menu.");
-        Console.ReadLine(); // pause the loop :)
+        
     }
     if (command == "2")
     {
@@ -98,9 +41,24 @@ while(true)
         Console.WriteLine("Press enter to return to main menu.");
         Console.ReadLine(); // pause the loop :)
     }
-    if (command == "3")
+    if (command == "6") // Exit
     {
         Console.WriteLine("Byebye");
         break;
     }
+}
+
+// list all products
+static void ShowAllProducts()
+{
+    Console.Clear();
+    Console.WriteLine("Our products:");
+
+    foreach (string product in products)
+    {
+        Console.WriteLine(product);
+    }
+
+    Console.WriteLine("Press enter to return to main menu.");
+    Console.ReadLine(); // pause the loop :)
 }
